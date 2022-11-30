c9c64c13296af4a59743dc29a412f9fb6c9ef526
JenDacres <119019863+JenDacres@users.noreply.github.com>
2022-11-30 08:17:43 -0500

    Update DLineAlert.java

--- src/APP/NotificationsandEvents/DLineAlert.java
+++ src/APP/NotificationsandEvents/DLineAlert.java
@@ -2,8 +2,35 @@ package APP.NotificationsandEvents;
 import java.util.*;
 
 public class DLineAlert {
- public static void main(String[] args) {
-     Date d=new Date();  
-     System.out.println(d.toString());
+public class dateline{
+  private int[]values;
+// This method should accpet 3 values representing the day,month and year respectively and should place these values into an array. This will be th current date.
+public int[]setDate(int a, int b, int c){
+    int day= a;
+    int monthss= b;
+    int year= c;
+    int[]values={a,b,c};
+    return values;
+  }
+}
+/*
+// This method shall accept an array containing a date, the days of this date willl be subtracted from each other the differnce will be calculated to see if its less 
+   or equal to or greater than 2, where a string shall return if the Deadline is appraoching or no.
+ 
+ public String void CompareDate(int[]date){
+  int sum1=date[0];
+  int sum2=values[0];
+  int sum3=sum2-sum1;
+  if (sum3 > 2){ 
+  System.out.println("NO DEADLINE") ;
+  }
+  else if (sum3<=2){
+    System.out.println("DEADLINE APPROACHING");
+  }
+  else{
+    System.out.println("ERROR");
+    */
+  }
+}
  }
 }
